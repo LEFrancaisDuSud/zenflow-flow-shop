311c5e0d90851233d4d0d33ae0fea2ae753569f2
LEFrancaisDuSud <lefrancaisdusud@gmail.com>
2026-06-23 17:44:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    cssMinify: false,
   }
 })
